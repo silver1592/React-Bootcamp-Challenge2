e0d594e8a783a4991812998ab05501d7ca3c6ba7
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-23 23:17:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>2o. Challenge: Loops</h1>
+      {/**
+        Render here the list of pokemons
+      */}
+    </div>
+  );
+}
